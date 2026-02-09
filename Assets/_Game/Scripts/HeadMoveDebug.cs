d04a02406a2d03dd04932492319401e89712ae36
eastrt <61608697+eastrt@users.noreply.github.com>
2026-02-09 09:20:30 +0900

    Debug Log to Comment

--- Assets/_Game/Scripts/HeadMoveDebug.cs
+++ Assets/_Game/Scripts/HeadMoveDebug.cs
@@ -4,8 +4,8 @@ public class DebugLocalRot : MonoBehaviour
 {
     void LateUpdate()
     {
-        Debug.Log($"[HeadPrefab F1] localY={transform.localEulerAngles.y:F1} worldY={transform.eulerAngles.y:F1}");
-        Debug.Log($"[HeadPrefab F2] localY={transform.localEulerAngles.y:F2} worldY={transform.eulerAngles.y:F2}");
+        //Debug.Log($"[HeadPrefab F1] localY={transform.localEulerAngles.y:F1} worldY={transform.eulerAngles.y:F1}");
+        //Debug.Log($"[HeadPrefab F2] localY={transform.localEulerAngles.y:F2} worldY={transform.eulerAngles.y:F2}");
         // 부모(HeadSocket)의 회전만 따르게 하고, 자신은 로컬 회전 고정
         transform.localRotation = Quaternion.identity;
         transform.localPosition = Vector3.zero;
